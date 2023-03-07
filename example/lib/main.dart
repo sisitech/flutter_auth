24b040cf359406ec3594f488f6a1cb12f20fbc6a
Mwangi Micha <michameiu@gmail.com>
2023-03-07 10:49:23 +0300

    Implement offline login

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/flutter_auth_controller.dart';
-import 'package:flutter_login/flutter_login.dart';
+import 'package:flutter_form/flutter_form.dart';
+import 'package:flutter_form/models.dart';
+import 'package:flutter_utils/flutter_utils.dart';
 import 'package:flutter_utils/models.dart';
+import 'package:flutter_utils/network_status/network_status_controller.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
@@ -15,8 +18,10 @@ void main() async {
       tokenUrl: 'o/token/',
       grantType: "password",
       revokeTokenUrl: 'o/revoke_token/'));
+  // await GetStorage.init(offline_login_storage_container);
   await GetStorage.init();
   Get.lazyPut(() => AuthController());
+  Get.put(NetworkStatusController());
   // StoreBinding();
   runApp(const MyApp());
 }
@@ -76,6 +81,72 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
+class LoginWidget extends StatelessWidget {
+  const LoginWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    APIConfig config = Get.find<APIConfig>();
+    AuthController authController = Get.find<AuthController>();
+    Map<String, dynamic>? offlineCred;
+    return MyCustomForm(
+      url: "o/token/",
+      submitButtonText: "Login".tr,
+      submitButtonPreText: "",
+      enableOfflineMode: true,
+      enableOfflineSave: false,
+      loadingMessage: "Signing in...".tr,
+      validateOfflineData: (data) async {
+        var res = await authController.confirmOfflineCreds(data);
+
+        if (res == null) {
+          return {"detail": "No offline credentials found".tr};
+        }
+        if (!res) {
+          return {"password": "Confirm password".tr};
+        } else {
+          dprint("Offline Authenticated $res");
+          dprint("${authController.offlineCred}");
+          return null;
+        }
+      },
+      instance: {
+        "username": "micha@sisitech.com",
+        "password": "mm",
+      },
+      onOfflineSuccess: (res) {
+        dprint("Login offline successful");
+        authController.unLock();
+      },
+      contentType: ContentType.form_url_encoded,
+      extraFields: {
+        "client_id": config.clientId,
+        "grant_type": config.grantType,
+      },
+      PreSaveData: (res) {
+        authController.loginForm.value = res;
+        //Save the login Form
+        return res;
+      },
+      onSuccess: (res) async {
+        dprint("Logged in");
+        dprint(res);
+        dprint("creds");
+        dprint(authController.loginForm.value);
+        await authController.saveOfflineCreds();
+        await authController.getSaveProfile(res);
+
+        return null;
+      },
+      formGroupOrder: const [
+        ["username"],
+        ["password"]
+      ],
+      formTitle: "Signup",
+    );
+  }
+}
+
 class HomePage extends StatelessWidget {
   HomePage({super.key});
   AuthController authController = Get.find<AuthController>();
@@ -88,6 +159,13 @@ class HomePage extends StatelessWidget {
         ElevatedButton(
           onPressed: logout,
           child: Text("Logout"),
+        ),
+        SizedBox(
+          height: 40,
+        ),
+        ElevatedButton(
+          onPressed: lock,
+          child: Text("Lock"),
         )
       ],
     );
@@ -95,5 +173,9 @@ class HomePage extends StatelessWidget {
 
   logout() async {
     await authController.logout();
+  }
+
+  lock() async {
+    await authController.lock();
   }
 }
--- lib/flutter_auth_controller.dart
+++ lib/flutter_auth_controller.dart
@@ -1,78 +1,146 @@
 library flutter_auth;
 
+import 'package:encrypt/encrypt.dart' as encrypt;
+import 'package:flutter_utils/flutter_utils.dart';
+
 import 'package:flutter_utils/models.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'auth_connect.dart';
+
+const offline_login_storage_container = "offline_login";
+const offline_password_key = "offline_password_key";
 
 class AuthController extends GetxController {
   var isAuthenticated$ = false.obs;
   final box = GetStorage();
   var config = Get.find<APIConfig>();
   var authProv = Get.put<AuthProvider>(AuthProvider());
+  var offline_box = GetStorage(offline_login_storage_container);
+
+  Rx<Map<String, dynamic>?> loginForm = Rx(null);
+
   Rx<Map<String, dynamic>?> profile = Rx(null);
+  Rx<Map<String, dynamic>?> offlineCred = Rx(null);
+
   @override
   void onInit() {
     // TODO: implement onInit
     super.onInit();
     checkloggedIn();
-    print("Auth controller init...");
+    dprint("Auth controller init...");
   }
 
   checkloggedIn() async {
     var token = await getToken();
     var read_profile = await getProfile();
-    print("The token is ");
-    print(token);
-    isAuthenticated$.value = token != null;
+    // Token is not null and lock is false
+    isAuthenticated$.value = token != null && !(await getLockStatus());
+    dprint(token);
+    dprint(
+        "toke is null ${token != null} and lock status ${await getLockStatus()}");
     profile.value = read_profile;
   }
 
-  saveToken(dynamic body) async {
-    print("The token is ");
-    await box.write('token', body);
+  Future<bool> getLockStatus() async {
+    var res = await box.read<bool>('locked') ?? true;
+    return Future.value(res);
+  }
 
-    // formPost();
+  saveToken(dynamic body) async {
+    await box.write('token', body);
+    return box.write('locked', false);
+  }
+
+  Map<String, dynamic> encryptPassword(String username, String password) {
+    // dprint("Encryptin $username $password");
+    final key = encrypt.Key.fromSecureRandom(32);
+    final iv = encrypt.IV.fromSecureRandom(16);
+    // dprint(key.base16);
+    // dprint(iv.base16);
+    final encrypter = encrypt.Encrypter(encrypt.AES(key));
+    final encrypted = encrypter.encrypt(password, iv: iv);
+    return {
+      "iv": iv.base64,
+      "password": encrypted.base64,
+      "key": key.base64,
+      "username": username
+    };
+  }
+
+  saveOfflineCreds() async {
+    if (loginForm.value != null) {
+      var encyptredPassword = encryptPassword(
+          loginForm.value?["username"], loginForm.value?["password"]);
+      // dprint("Saving");
+      // dprint(encyptredPassword);
+      await offline_box.write(offline_password_key, encyptredPassword);
+    }
+  }
+
+  confirmOfflineCreds(form) async {
+    var res = await offline_box.read(offline_password_key);
+    if (res == null) {
+      return null;
+    }
+    var username = form?["username"];
+    var password = form?["password"];
+    offlineCred.value = res;
+    final key = encrypt.Key.fromBase64(res["key"]);
+    final iv = encrypt.IV.fromBase64(res["iv"]);
+    final encrypter = encrypt.Encrypter(encrypt.AES(key));
+    final encryptedPassword = encrypter.encrypt(password, iv: iv);
+    return res["password"] == encryptedPassword.base64 &&
+        res["username"] == username;
+  }
+
+  lock() async {
+    await box.write('locked', true);
+    checkloggedIn();
+  }
+
+  unLock() async {
+    await box.write('locked', false);
+    checkloggedIn();
   }
 
   logout() async {
-    print("Logout");
+    dprint("Logout");
     //  const url = `${this.config.APIEndpoint}/${this.config.revokeTokenUrl}`
     var token = await getToken();
-
-    print(token);
+    dprint(token);
     try {
       var data = {"token": token["access_token"], "client_id": config.clientId};
-      print(data);
-      print(config.revokeTokenUrl);
+      dprint(data);
+      dprint(config.revokeTokenUrl);
       var response =
           await authProv.formPostUrlEncoded(config.revokeTokenUrl, data);
-      print(response);
-      print(response.statusCode);
-      print(response.body);
+      dprint(response);
+      dprint(response.statusCode);
+      dprint(response.body);
       if (response.statusCode == 200) {}
       await box.remove('token');
       checkloggedIn();
     } catch (e) {
-      print(e);
+      dprint(e);
     }
   }
 
   getSaveProfile(dynamic token) async {
-    saveToken(token);
+    await saveToken(token);
     authProv.onInit();
     try {
       var received_profile = await authProv.formGet(config.profileUrl);
-      print(received_profile.statusCode);
-      print(received_profile.body);
+      dprint(received_profile.statusCode);
+      dprint(received_profile.body);
       if (received_profile.statusCode == 200) {
-        profile = received_profile.body;
+        profile.value = received_profile.body;
         await saveProfile(received_profile.body);
       }
       checkloggedIn();
       return;
     } catch (e) {
-      print(e);
+      dprint(e);
       return;
     }
   }
